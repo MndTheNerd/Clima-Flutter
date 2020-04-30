095f8868fdff03cf75e7bf282c22234f4646a4c3
MndTheNerd <mndthenerd@gmail.com>
2020-04-30 03:03:23 +0300

    adding a loading animation and importing SpinKit package for it

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '22e21a7ab6b0906bc6ca9e022e3ebf29';
 
@@ -40,6 +41,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
